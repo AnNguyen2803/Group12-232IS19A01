1cba91d4c566241938285e91fcf4c95354ae7d49
annguyen2803 <nguyenanht2003@gmail.com>
2024-04-23 20:43:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,157 @@
+// carousel
+    $('.slider-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplayTimeout: 2000,
+    dots: true,
+    nav: true,
+    navContainer: '.owl-nav',
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+    });
+prdCarousel();
+function prdCarousel(){
+    $(".product-carousel").owlCarousel({
+        autoplay: false,
+        center: false,
+        margin: 20,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsiveClass: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
+}
+
+$('.slider-ads-brand').owlCarousel({
+    loop:true,
+    margin:30,
+    autoplayTimeout: 2000,
+    nav: true,
+    navText : [
+        '<i class="bi bi-arrow-left"></i>',
+        '<i class="bi bi-arrow-right"></i>'
+    ],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+    });
+    $('.list-gallery').owlCarousel({
+        loop:true,
+        margin:30,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+        });
+// icon dropDown
+   
+const helpIcon = document.querySelector('.bi-headphones');
+const subAction = document.querySelector('.sub-action');
+
+helpIcon.addEventListener("click", function(e){
+    e.preventDefault();
+    subAction.classList.toggle('db');
+})
+
+/// toggle product tab
+
+const exclusiveTab = document.querySelectorAll(".home-new-products .exclusive-tab");
+const innerExclusive = document.querySelectorAll(".home-new-products .exclusive-inner");
+const carousel = document.querySelectorAll(".home-new-products .exclusive-inner .owl-carousel");
+
+exclusiveTab.forEach((tab, index) => {
+    console.log(tab);
+    tab.onclick = function(){
+        document.querySelector(".home-new-products .exclusive-tab.active").classList.remove("active");
+        tab.classList.add("active");
+        HandleExclusiveTab(index);
+        HandleCarousel(index);
+    }
+
+})
+function HandleExclusiveTab(index){
+    var tabProduct= innerExclusive[index];
+    document.querySelector(".home-new-products .exclusive-inner.active").classList.remove("active");
+    tabProduct.classList.add("active");
+}
+function HandleCarousel(index){
+    var crs = carousel[index];
+    document.querySelector(".home-new-products .owl-carousel.product-carousel").classList.remove("product-carousel");
+    crs.classList.add("product-carousel");
+    prdCarousel();
+}
+// Best seller tab
+const exclusiveTabSeller = document.querySelectorAll(".home-new-products-seller .exclusive-tab");
+const innerExclusiveSeller = document.querySelectorAll(".home-new-products-seller .exclusive-inner");
+const carouselSeller = document.querySelectorAll(".home-new-products-seller .owl-carousel");
+
+
+exclusiveTabSeller.forEach((tab, index) => {
+    tab.onclick = function(){
+        document.querySelector(".home-new-products-seller .exclusive-tab.active").classList.remove("active");
+        tab.classList.add("active");
+        HandleExclusiveTabSeller(index);
+        HandleCarouselSeller(index);
+    }
+})
+function HandleExclusiveTabSeller(index){
+    var tabProduct= innerExclusiveSeller[index];
+    document.querySelector(".home-new-products-seller .exclusive-inner.active").classList.remove("active");
+    tabProduct.classList.add("active");
+}
+function HandleCarouselSeller(index){
+    var crs = carouselSeller[index];
+    document.querySelector(".home-new-products-seller .owl-carousel.product-carousel").classList.remove("product-carousel");
+    crs.classList.add("product-carousel");
+    prdCarousel();
+}
+
